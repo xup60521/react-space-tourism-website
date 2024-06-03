e93dbf4099c1a05cb94eb9a4bd8f384e6c31f891
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-06-03 21:27:50 +0800

    mobile nav menu

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,14 @@
 import { NavLink } from "react-router-dom";
 import { route } from "../route";
 import Logo from "/assets/shared/logo.svg";
+import Menu from "/assets/shared/icon-hamburger.svg"
+import { useState } from "react";
 
 export default function Nav() {
+    const [openMenu, setOpenMenu] = useState(false)
     return (
         <div className="w-full flex z-30 py-12 items-center overflow-x-hidden">
-            <div className="flex justify-center pl-16 h-full items-center">
+            <div className="flex justify-center md:pl-16 pl-12 h-fit items-center">
                 <img src={Logo} className="size-[3.5rem]" />
             </div>
             <div className="flex-grow h-[1px] lg:bg-neutral-800 bg-transparent translate-x-[2.5rem] z-[100] "></div>
@@ -34,6 +37,34 @@ export default function Nav() {
                     </NavLink>
                 ))}
             </nav>
+            <button className="md:hidden flex items-center pr-12" onClick={()=>setOpenMenu(!openMenu)}>
+                <img src={Menu} className="size-[2rem]" alt="menu-icon" />
+            </button>
+            {openMenu && <div className="absolute top-[9.5rem] w-[90vw] left-[50%] -translate-x-[50%] border-2-white rounded-3xl  flex flex-col gap-4 p-4 md:hidden items-center backdrop-blur-2xl backdrop-brightness-125">
+            {route.map((item, index) => (
+                    <NavLink
+                        to={item.path}
+                        key={item.path}
+                        unstable_viewTransition
+                        onClick={()=>setOpenMenu(false)}
+                        className={(c) => {
+                            let className =
+                                "text-white h-full md:py-8 py-4 box-border border-b-4 transition flex items-center gap-3 w-fit ";
+                            if (c.isActive) {
+                                className += "border-white ";
+                            } else {
+                                className += "border-transparent ";
+                            }
+                            return className;
+                        }}
+                    >
+                        <span className="font-bold lg:block hidden">
+                            {index.toString().padStart(2, "0")}
+                        </span>
+                        <span className="font-thin lg:text-xl">{` ${item.name}`}</span>
+                    </NavLink>
+                ))}
+                </div>}
         </div>
     );
 }
--- src/components/technology/technology.tsx
+++ src/components/technology/technology.tsx
@@ -6,7 +6,7 @@ export default function Technology() {
             initial={{ opacity: 0 }}
             animate={{ opacity: "100%" }}
             exit={{ opacity: 0 }}
-            className="absolute top-0 left-0 w-screen h-fit min-h-screen lg:bg-[url('/assets/technology/background-technology-desktop.jpg')] bg-cover md:bg-[url('/assets/technology/background-technology-tablet.jpg')] bg-[url('/assets/technology/background-technology-mobile.jpg')] overflow-x-hidden"
+            className="absolute top-0 left-0 w-full h-fit min-h-screen lg:bg-[url('/assets/technology/background-technology-desktop.jpg')] bg-cover md:bg-[url('/assets/technology/background-technology-tablet.jpg')] bg-[url('/assets/technology/background-technology-mobile.jpg')] overflow-x-hidden"
         ></motion.main>
     );
 }
